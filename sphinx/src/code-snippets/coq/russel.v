7876561a8a15794a984c36a8dca459e879091786
hbr <helmut.brandl@gmx.net>
2023-02-18 17:32:06 -0600

    Made names more expressive
    	modified:   sphinx/src/code-snippets/coq/russel.v

--- sphinx/src/code-snippets/coq/russel.v
+++ sphinx/src/code-snippets/coq/russel.v
@@ -1,37 +1,39 @@
 Inductive RSet@{i} : Type@{i} :=
     ens_intro : forall A : Type@{i}, (A -> RSet) -> RSet.
 
-Definition idx (A : RSet) : Type :=
-    match A with
+Definition TypeOf (s : RSet) : Type :=
+    match s with
         ens_intro T _ => T
     end.
 
-Definition elem (S : RSet) (i : idx S) : RSet :=
+(* Element of 'S' where 'i' is an object of 'TypeOf S'. *)
+Definition elem (s : RSet) (i : TypeOf s) : RSet :=
     match
-        S as S' return idx S' -> RSet
+        s as s' return TypeOf s' -> RSet
     with
-        ens_intro A f => (fun (j: A) => f j)
+        ens_intro A f => f
     end i.
 
-(*
-Definition elem_of (S1 S2: RSet): Prop :=
-    match S2 with
-        ens_intro A f =>
-            _
-    end.
-*)
 
-Definition elem_of (x A : RSet): Prop :=
-    exists a, elem A a = x.
+
+(* Is s1 an element of s2? *)
+Definition IsElem (s1 s2 : RSet): Prop :=
+    exists a: TypeOf s2, elem s2 a = s1.
+
+
+
 
 Definition Union {T: Type} (f : T -> RSet): RSet :=
     ens_intro
-        {x : T & idx (f x)}
+        {x : T & TypeOf (f x)} (* Pairs (x: T, y: TypeOf (f x)) *)
         (fun u => elem (f (projT1 u)) (projT2 u)).
 
+
+
+
 Lemma
-    elem_of_union {T} (f : T -> RSet) A
-    : elem_of A (Union f) <-> exists t, elem_of A (f t).
+    IsElem_union {T} (f : T -> RSet) (s: RSet)
+    : IsElem s (Union f) <-> exists t, IsElem s (f t).
 Proof.
  split.
  - intros [[z idx] Hzidx]; exists z, idx; assumption.
@@ -39,11 +41,15 @@ Proof.
 Qed.
 
 
-Definition singleton_if (a : RSet) (P : Prop) : RSet :=
-    ens_intro {x : unit | P} (fun _ => a).
+
+
+Definition singleton_if (s : RSet) (P : Prop) : RSet :=
+    ens_intro {_ : unit | P} (fun _ => s).
+
+
 
 Lemma
-    elem_of_singleton_if a b P : (P /\ a = b) <-> elem_of b (singleton_if a P).
+    IsElem_singleton_if (a b: RSet) (P: Prop) : (P /\ a = b) <-> IsElem b (singleton_if a P).
 Proof.
  split.
  - intros [HP ->].
@@ -51,19 +57,32 @@ Proof.
  - intros [[z Hz1] Hz2]; tauto.
 Qed.
 
+
+
 Definition Russel : RSet :=
-    Union (fun A : RSet => singleton_if A (not (elem_of A A))).
+    Union (fun A : RSet => singleton_if A (not (IsElem A A))).
+
+
 
 Theorem
-    Russel's_Paradox : elem_of Russel Russel <-> not (elem_of Russel Russel).
+    Russel's_Paradox : IsElem Russel Russel <-> not (IsElem Russel Russel).
 Proof.
  split.
  - intros Helem.
-   apply elem_of_union in Helem as [t Ht].
-   apply elem_of_singleton_if in Ht as [? ->].
+   apply IsElem_union in Helem as [t Ht].
+   apply IsElem_singleton_if in Ht as [? ->].
    assumption.
  - intros Hnelem.
-   apply elem_of_union.
+   apply IsElem_union.
    exists Russel.
-   apply elem_of_singleton_if; split; [assumption|reflexivity].
+   apply IsElem_singleton_if; split; [assumption|reflexivity].
 Qed.
+
+
+Lemma absurd: False.
+Proof.
+ generalize Russel's_Paradox. intuition.
+Qed.
+
+
+(*Print absurd.*)
